3984077b5aee13a182a277144d6a0c14d223a319
thesmizen <sean@seanmizen.com>
2020-06-15 22:26:01 +0100

    Final commit, moving to new proj

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ time = 500;
 let timer2 = setInterval(function(){
     console.log(time + " milliseconds have passed in setInterval")
     time += 2;
-    if (time > 510){
+    if (time > 505){
         clearInterval(timer2);
     }
 }, time);
@@ -21,3 +21,19 @@ let timer3 = setTimeout(function(){ //setTimeout runs once
 //They will all fire only as and when their timers run out.
 
 console.log(__filename + " starting in " + __dirname);
+
+//2020-06-15
+//Payday woop
+//Today: Function expressions
+function callFunction(fnc){
+    fnc();
+}
+function sayHi(){
+    console.log("hi");
+}
+sayHi();
+var sayBye = function(){
+    console.log("bye");
+};
+callFunction(sayBye);
+//Starting a new project so this starts to make sense.
