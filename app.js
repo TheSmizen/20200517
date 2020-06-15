b2edd45071e97a1f501b097f47c9e3015ff2de84
thesmizen <sean@seanmizen.com>
2020-06-15 22:17:35 +0100

    Oh that's what async does

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 console.log("Node project starting. " + Date());
-let time = 500;
+let time = 501;
 //global object:
 let timer = setTimeout(function(){ //setTimeout runs once
     time += 2;
     console.log(time + " milliseconds have passed in setTimeout");
-},time)
+}, time)
 time = 500;
 let timer2 = setInterval(function(){
     console.log(time + " milliseconds have passed in setInterval")
@@ -12,8 +12,12 @@ let timer2 = setInterval(function(){
     if (time > 510){
         clearInterval(timer2);
     }
-},time);
+}, time);
+let timer3 = setTimeout(function(){ //setTimeout runs once
+    console.log("2000 milliseconds have passed in setTimeout");
+}, 2000);
 
-//.
+//Here in app.js all three timers get launched at the same time.
+//They will all fire only as and when their timers run out.
 
-console.log("App starting");
+console.log(__filename + " starting in " + __dirname);
